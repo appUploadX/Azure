bf5d26862a40d61613b69e384b28fbdc84141373
Erick Ryan <erickperez.agsi@gmail.com>
2019-12-02 17:20:32 +0800

    Changes

--- azure/src/app/visitor-add-request/visitor-add-request.page.ts
+++ azure/src/app/visitor-add-request/visitor-add-request.page.ts
@@ -77,6 +77,8 @@ export class VisitorAddRequestPage implements OnInit {
 	minDepTime: string;
 	maxDate: string = (new Date().getFullYear() + 1) + "-12-31";
 	ErrorDateTime = 0;
+	ErrorDateTimeArr = 0;
+	ErrorDateTimeDep = 0;
 	constructor(
 		private modalController: ModalController,
 		private postPvd: PostProvider,
@@ -209,43 +211,41 @@ export class VisitorAddRequestPage implements OnInit {
 	arriveChange(val) {
 		var value = val.split('T');
 		this.minDepTime = value[0];
-		
-		if($("#vtArrivalTime").val() != "")
-		{
+
+		if ($("#vtArrivalTime").val() != "") {
 			this.arriveTimeChange(this.vtArrivalTime);
 		}
+
 	}
 
 	arriveTimeChange(value) {
-		console.log (this.vtArrivalDate);
+		console.log(this.vtArrivalDate);
 		var expDate = this.vtArrivalDate.split("T");
 		var expTime = value.split("T");
 		var expTimeSplit = expTime[1].split(":");
 
-		var date = expDate[0]+"T"+expTimeSplit[0]+":"+expTimeSplit[1]+":00.000+08:00";
-		this.timeChange(date);
+		var date = expDate[0] + "T" + expTimeSplit[0] + ":" + expTimeSplit[1] + ":00.000+08:00";
+		this.ArrtimeChange(date);
 	}
 
 	departChange(val) {
 		var value = val.split('T');
-		if($("#vtDepartureTime").val() != "")
-		{
+		if ($("#vtDepartureTime").val() != "") {
 			this.departTimeChange(this.vtDepartureTime);
 		}
 	}
 
 	departTimeChange(value) {
-		console.log (this.vtDepartureDate);
+		console.log(this.vtDepartureDate);
 		var expDate = this.vtDepartureDate.split("T");
 		var expTime = value.split("T");
 		var expTimeSplit = expTime[1].split(":");
 
-		var date = expDate[0]+"T"+expTimeSplit[0]+":"+expTimeSplit[1]+":00.000+08:00";
-		this.timeChange(date);
+		var date = expDate[0] + "T" + expTimeSplit[0] + ":" + expTimeSplit[1] + ":00.000+08:00";
+		this.DeptimeChange(date);
 	}
 
-	timeChange(date)
-	{
+	ArrtimeChange(date) {
 		console.log(date);
 		return new Promise(resolve => {
 			let body = {
@@ -258,11 +258,81 @@ export class VisitorAddRequestPage implements OnInit {
 			this.postPvd.postData(body, 'https://www.asi-ph.com/sandboxes/testAndroid/CondoProcess/').subscribe(data => {
 				if (data['status'] == 'Allowed') {
 					this.openToast('<center>Available!</center>');
+					this.ErrorDateTimeArr = 0;
+					console.log(this.ErrorDateTimeArr + "AllowedARR");
+				}
+				else {
+					this.openToast('<center>Error! You already have a guest/s on these date and time.</center>');
+					this.ErrorDateTimeArr = 1;
+					console.log(this.ErrorDateTimeArr + "Error");
+				}
+				resolve(true);
+				console.log(data['status']);
+			});
+		});
+	}
+
+	DeptimeChange(date) {
+		console.log(date);
+		return new Promise(resolve => {
+			let body = {
+				action: 'checkRequest',
+				unit_code: this.uCode,
+				newCode: this.newCode,
+				departureTime: date,
+			};
+
+			this.postPvd.postData(body, 'https://www.asi-ph.com/sandboxes/testAndroid/CondoProcess/').subscribe(data => {
+				if (data['status'] == 'Allowed') {
+					this.openToast('<center>Available!</center>');
+					this.ErrorDateTimeDep = 0;
+					console.log(this.ErrorDateTimeDep + "AllowedDEP");
+				}
+				else {
+					this.openToast('<center>Error! You already have a guest/s on these date and time.</center>');
+					this.ErrorDateTimeDep = 1;
+					console.log(this.ErrorDateTimeDep + "NotAllowedDEP");
+
+				}
+				resolve(true);
+				console.log(data['status']);
+			});
+		});
+	}
+
+	CKSubmitTime() {
+		//Arrive
+		var expDateArr = this.vtArrivalDate.split("T");
+		var expTimeArr = this.vtArrivalTime.split("T");
+		var expTimeSplitArr = expTimeArr[1].split(":");
+		var dateArr = expDateArr[0] + "T" + expTimeSplitArr[0] + ":" + expTimeSplitArr[1] + ":00.000+08:00";
+		//Depart
+		var expDateDep = this.vtDepartureDate.split("T");
+		var expTimeDep = this.vtDepartureTime.split("T");
+		var expTimeSplitDep = expTimeDep[1].split(":");
+
+		var dateDep = expDateDep[0] + "T" + expTimeSplitDep[0] + ":" + expTimeSplitDep[1] + ":00.000+08:00";
+
+		return new Promise(resolve => {
+			let body = {
+				action: 'checkRequestAll',
+				unit_code: this.uCode,
+				newCode: this.newCode,
+				ArrTime: dateArr,
+				DepTime: dateDep,
+			};
+
+			this.postPvd.postData(body, 'https://www.asi-ph.com/sandboxes/testAndroid/CondoProcess/').subscribe(data => {
+				if (data['status'] == 'Allowed') {
+					this.openToast('<center>Available!</center>');
 					this.ErrorDateTime = 0;
+					console.log(this.ErrorDateTime + "AllowedDEP");
 				}
 				else {
 					this.openToast('<center>Error! You already have a guest/s on these date and time.</center>');
 					this.ErrorDateTime = 1;
+					console.log(this.ErrorDateTime + "NotAllowedDEP");
+
 				}
 				resolve(true);
 				console.log(data['status']);
@@ -272,6 +342,7 @@ export class VisitorAddRequestPage implements OnInit {
 
 	submit() {
 		var count = 0;
+		this.CKSubmitTime();
 		$(".checked").each(function () {
 			if ($(this).val() == "") {
 				count++;
@@ -320,51 +391,82 @@ export class VisitorAddRequestPage implements OnInit {
 		if (count == 0 && (countVal == $("#additional").val() || $("#additional").val() == "") && (countSel == $("#additional").val() || $("#additional").val() == "") && count_check != 0) {
 			var pattern = /^[\w\-\.\+]+\@[a-zA-Z0-9\.\-]+\.[a-zA-z0-9]{2,4}$/;
 			if (pattern.test($(".email").val())) {
-				if(this.ErrorDateTime === 0)
-				{
+				if (this.ErrorDateTimeDep === 0 && this.ErrorDateTimeArr === 0) {
+					var expDateArr = this.vtArrivalDate.split("T");
+					var expTimeArr = this.vtArrivalTime.split("T");
+					var expTimeSplitArr = expTimeArr[1].split(":");
+					var dateArr = expDateArr[0] + "T" + expTimeSplitArr[0] + ":" + expTimeSplitArr[1] + ":00.000+08:00";
+					//Depart
+					var expDateDep = this.vtDepartureDate.split("T");
+					var expTimeDep = this.vtDepartureTime.split("T");
+					var expTimeSplitDep = expTimeDep[1].split(":");
+
+					var dateDep = expDateDep[0] + "T" + expTimeSplitDep[0] + ":" + expTimeSplitDep[1] + ":00.000+08:00";
+
 					return new Promise(resolve => {
 						let body = {
-							action: 'addVisitors',
-							vuVisitorType: this.label,
-							vtUnitOwner: this.fullname,
-							vtTowerUnit: this.TUN,
-							vtCarparkSlotNo: this.vtCarparkSlotNo,
-							vtGuestOnSite: this.vtGuestOnSite,
-							vtGuestContact: this.vtGuestContact,
-							vtArrivalDate: this.vtArrivalDate,
-							vtArrivalTime: this.vtArrivalTime,
-							vtDepartureDate: this.vtDepartureDate,
-							vtDepartureTime: this.vtDepartureTime,
-							vuNamePrimaryVisitor: this.vtPrimaryVisitorName,
-							vtPrimaryVisitorNationality: this.vtPrimaryVisitorNationality,
-							vtPrimaryVisitorContactNo: this.vtPrimaryVisitorContactNo,
-							vtPrimaryVisitorEmailAddress: this.vtPrimaryVisitorEmailAddress,
-							vtPrimaryVisitorAddress: this.vtPrimaryVisitorAddress,
-							vtAdditionalVisitorCount: this.vtAdditionalVisitorCount,
-							vtVehicleDetailsCount: this.vtVehicleDetailsCount,
-							vtRemarks: this.vtRemarks,
-							vtRemarksByAdmin: this.vtRemarksByAdmin,
-							condition: this.condition,
-							vuUnitCode: this.uCode,
-							ownerCode: this.newCode,
-	
-							avName: this.visitC,
-							vehicles: this.vehicles
+							action: 'checkRequestAll',
+							unit_code: this.uCode,
+							newCode: this.newCode,
+							ArrTime: dateArr,
+							DepTime: dateDep,
 						};
-	
+
 						this.postPvd.postData(body, 'https://www.asi-ph.com/sandboxes/testAndroid/CondoProcess/').subscribe(data => {
-							if (data['status'] == "Success") {
-								this.openToast("<center>Data succesfully saved!</center>");
-								setTimeout(() => { this.router.navigateByUrl('/tabs/tab1/visitors-details') }, 2000)
+							if (data['status'] == 'Allowed') {
+								return new Promise(resolve => {
+									let body = {
+										action: 'addVisitors',
+										vuVisitorType: this.label,
+										vtUnitOwner: this.fullname,
+										vtTowerUnit: this.TUN,
+										vtCarparkSlotNo: this.vtCarparkSlotNo,
+										vtGuestOnSite: this.vtGuestOnSite,
+										vtGuestContact: this.vtGuestContact,
+										vtArrivalDate: this.vtArrivalDate,
+										vtArrivalTime: this.vtArrivalTime,
+										vtDepartureDate: this.vtDepartureDate,
+										vtDepartureTime: this.vtDepartureTime,
+										vuNamePrimaryVisitor: this.vtPrimaryVisitorName,
+										vtPrimaryVisitorNationality: this.vtPrimaryVisitorNationality,
+										vtPrimaryVisitorContactNo: this.vtPrimaryVisitorContactNo,
+										vtPrimaryVisitorEmailAddress: this.vtPrimaryVisitorEmailAddress,
+										vtPrimaryVisitorAddress: this.vtPrimaryVisitorAddress,
+										vtAdditionalVisitorCount: this.vtAdditionalVisitorCount,
+										vtVehicleDetailsCount: this.vtVehicleDetailsCount,
+										vtRemarks: this.vtRemarks,
+										vtRemarksByAdmin: this.vtRemarksByAdmin,
+										condition: this.condition,
+										vuUnitCode: this.uCode,
+										ownerCode: this.newCode,
+
+										avName: this.visitC,
+										vehicles: this.vehicles
+									};
+
+									this.postPvd.postData(body, 'https://www.asi-ph.com/sandboxes/testAndroid/CondoProcess/').subscribe(data => {
+										if (data['status'] == "Success") {
+											this.openToast("<center>Data succesfully saved!</center>");
+											setTimeout(() => { this.router.navigateByUrl('/tabs/tab1/visitors-details') }, 2000)
+										}
+									})
+								});
+								// this.openToast('<center>Allowed.</center>');
+
 							}
-						})
+							else {
+								this.openToast('<center>Error! You already have a guest/s on these date and time.</center>');
+
+							}
+							// resolve(true);
+							console.log(data['status']);
+						});
 					});
 				}
-				else
-				{
+				else {
 					this.openToast('<center>Error! You already have a guest/s on these date and time.</center>');
 				}
-				
+
 			}
 			else {
 				$(".email").css({ "border": '#FF0000 1px solid' });
