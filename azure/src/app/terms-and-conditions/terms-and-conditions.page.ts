799743d31935cca53db547c687ce48c01c4db51d
Erick Ryan <erickperez.agsi@gmail.com>
2019-12-05 16:32:11 +0800

    Changes

--- azure/src/app/terms-and-conditions/terms-and-conditions.page.ts
+++ azure/src/app/terms-and-conditions/terms-and-conditions.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router} from '@angular/router';
 
 @Component({
   selector: 'app-terms-and-conditions',
@@ -7,10 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TermsAndConditionsPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
     console.log(localStorage);
   }
 
+  closeModal() {
+    this.router.navigateByUrl('/tabs/tab1');
+  }
 }
